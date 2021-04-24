43419c37bb57aa51badfa8287de3657cbed20983
LazTheArtist <kingofcreepersyt@gmail.com>
2021-04-25 01:15:26 +0200

    moved around logic sides

--- src/main/java/multiteam/rethinkers/main/EventHandler.java
+++ src/main/java/multiteam/rethinkers/main/EventHandler.java
@@ -6,8 +6,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -29,21 +30,41 @@ public class EventHandler {
         PlayerEntity playerEntity = event.getPlayer();
 
         if(event.getClass() == PlayerInteractEvent.RightClickItem.class || event.getClass() == PlayerInteractEvent.RightClickBlock.class){
-            if(!worldIn.isClientSide){
                 if(usedItem.getItem() == TinkerSmeltery.copperCan.get() && clickedBlock.getBlock() != Blocks.AIR){
                     if(clickedBlock.getBlockState().isCollisionShapeFullBlock(worldIn.getChunkForCollisions(ChunkPos.getX((long)clickedPos.getX()), ChunkPos.getZ((long)clickedPos.getZ())), clickedPos)){
+                        BlockPos placepos;
+                        SoundEvent placeSound = ModBlocks.COPPER_CAN_BLOCK.get().getSoundType(ModBlocks.COPPER_CAN_BLOCK.get().defaultBlockState()).getPlaceSound();
                         if(event.getFace() == Direction.UP){
-                            worldIn.setBlockAndUpdate(clickedPos.above(), ModBlocks.COPPER_CAN_BLOCK.get().defaultBlockState());
+                            placepos = clickedPos.above();
+                            if(worldIn.isClientSide){
+                                worldIn.playSound((PlayerEntity)null, placepos.getX(), placepos.getY(), placepos.getZ(), placeSound, SoundCategory.BLOCKS, 1f, 1f);
+                            }else{
+                                worldIn.setBlockAndUpdate(placepos, ModBlocks.COPPER_CAN_BLOCK.get().defaultBlockState());
+                                consumeItemIfIsNotCreative(playerEntity, usedItem);
+                            }
                         }else if(event.getFace() != Direction.DOWN && event.getFace() != Direction.UP){
-                            worldIn.setBlockAndUpdate(clickedPos.relative(event.getFace()), ModBlocks.COPPER_CAN_BLOCK.get().defaultBlockState());
+                            placepos = clickedPos.relative(event.getFace());
+                            if(worldIn.isClientSide){
+                                worldIn.playSound((PlayerEntity)null, placepos.getX(), placepos.getY(), placepos.getZ(), placeSound, SoundCategory.BLOCKS, 1f, 1f);
+                            }else{
+                                worldIn.setBlockAndUpdate(placepos, ModBlocks.COPPER_CAN_BLOCK.get().defaultBlockState());
+                                consumeItemIfIsNotCreative(playerEntity, usedItem);
+                            }
                         }
                     }
                 }
-            }
 
         }
 
+    }
 
+    public static void consumeItemIfIsNotCreative(PlayerEntity playerEntity, ItemStack stack){
+        if(!playerEntity.isCreative()){
+            stack.shrink(1);
+            if(stack.isEmpty()){
+                playerEntity.inventory.removeItem(stack);
+            }
+        }
     }
 
 }
